48e331612eefdef5a8152e5090d493d98c078287
codisart <louis.celier@gmail.com>
2017-09-07 23:11:26 +0200

    Extract class refactoring

--- src/Application/Factory.php
+++ src/Application/Factory.php
@@ -1,14 +1,24 @@
 <?php
+
 namespace NightWatch\Application;
 
 use NightWatch\Command\Factory\Watch as WatchFactory;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 
 class Factory
 {
     public function __invoke($config)
     {
+        /** @var Command $nightwatchCommand */
         $nightwatchCommand = (new WatchFactory)($config);
+        $nightwatchCommand->addOption(
+            'dry-run',
+            null,
+            InputOption::VALUE_NONE,
+            'Only shows which packages would have been updated'
+        );
 
         $application = new Application;
         $application->add($nightwatchCommand);
